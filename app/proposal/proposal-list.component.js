78f195a390ca6b503d45838554eb4d43e1639a13
Devan Walker <devanrwalker@gmail.com>
2017-04-17 09:41:20 -0600

    added sample data to proposals

--- app/proposal/proposal-list.component.js
+++ app/proposal/proposal-list.component.js
@@ -7,7 +7,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
+const proposal_1 = require("./proposal");
 let ProposalListComponent = class ProposalListComponent {
+    constructor() {
+        this.proposalOne = new proposal_1.Proposal(15, 'Company', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com');
+        this.proposalTwo = new proposal_1.Proposal(100, 'Company2', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com');
+        this.proposalThree = new proposal_1.Proposal(2, 'Company3', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
+    }
 };
 ProposalListComponent = __decorate([
     core_1.Component({
--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Company', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com')
+	proposalTwo: Proposal = new Proposal(100, 'Company2', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com')
+	proposalThree: Proposal = new Proposal(2, 'Company3', 'http://devcamp.com', 'Ruby on Rails', 150, 120, 15, 'devanrwalker@gmail.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne,
+		this.proposalTwo,
+		this.proposalThree
+	]
+}
